3a095e01721bad54b29acacd942d2ad2152b00cb
Charbel t <charbel2000.52@gmail.com>
2020-12-13 06:00:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,29 @@ class Cat{
     this.name=name;
     this.sex=sex;
   }
+  speak(){
+    return `${this.name} says meow!`;
+  }
+}
+
+class Dog{
+  constructor(name,sex){
+    this.name=name;
+    this.sex=sex;
+  }
+  speak(){
+    return `${this.name} says woof!`;
+  }
+}
+
+class Bird{
+  constructor(name,sex){
+    this.name=name;
+    this.sex=sex;
+  }
+  speak(){
+    if(this.sex=='male')
+    return `it's me! ${this.name} the parrot!`;
+    else ``
+  }
 }
